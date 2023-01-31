a7485eb709b1b5cf0a75d2a5e4b0fae9ae7150ae
ins-fox <test.com>
2023-01-31 08:25:08 +0000

    test

--- xor_find_key.py
+++ xor_find_key.py
@@ -3,17 +3,50 @@ import sys
 
 args = sys.argv
 print(args[1]) # clear text
-print(args[2]) # encrypted text
+f = open(args[1], 'r')
 
-clear_text = args[2].encode()
-encrypted_text = args[1]
-encrypted_raw = base64.b64decode(encrypted_text)
+datalist = f.readlines()
+count = 0
+for i in datalist:
+    hex_str = (i.rstrip()).split(':')
+    for data in hex_str:
+        hex_data = data.split(' ')[1:]
+        if count == 0:
+            p = hex_data
+        elif count == 1:
+            cs = hex_data
+        else:
+            cl = hex_data
+    
+    count = count+1
 
+print(p)
+print(cs)
+print(cl)
 
-find_key = []
-for i in range(len(clear_text)):
-    dec = encrypted_raw[i] ^ clear_text[i]
-    find_key = chr(dec)
-    fingd_key.append(find_key)
+k = 0
+key = []
+for i in p:
+    x16_num = int(i, 16) ^ int(cs[k], 16)
+    key.append(x16_num)
+    k = k + 1
 
-print("".join(find_key))
+for item in key:
+    print(chr(item), end=' ')
+    
+print('')
+
+k = 0
+ans = []
+for i in cl:
+    x16_num = int(i, 16) ^ int(key[k])
+    ans.append(x16_num)
+    key.append(key[k])
+    k = k + 1
+
+print(ans)
+for item in ans:
+    print(chr(item), end = '')
+
+print('')
+
